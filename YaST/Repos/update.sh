616c1b29287d9e4f2cacded246ed2bd48366adb9
Stephan Kulow <coolo@suse.de>
2007-09-17 14:30:50 +0000

    making them translatable

---
+++ YaST/Repos/update.sh
@@ -0,0 +1,23 @@
+for i in *.xml.in; do 
+  intltool-extract --type=gettext/xml $i
+done
+svn up $MY_LCN_CHECKOUT/50-pot
+xgettext -kN_ -o $MY_LCN_CHECKOUT/50-pot/community-repositories.pot *.h
+svn diff $MY_LCN_CHECKOUT/50-pot/community-repositories.pot
+svn ci $MY_LCN_CHECKOUT/50-pot/community-repositories.pot
+rm *.h
+
+svn up $MY_LCN_CHECKOUT
+rm -rf po
+mkdir po
+for i in $MY_LCN_CHECKOUT/*/po/community-repositories*.po; do 
+  file=`basename $i .po`
+  file=${file/community-repositories./}
+  cp $i po/$file.po
+done
+
+for i in *.xml; do 
+  intltool-merge --xml-style po $i.in $i
+done
+
+rm -rf po
