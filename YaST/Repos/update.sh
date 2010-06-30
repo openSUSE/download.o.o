a052dcee89b429dffbb89e09c22d7eeb325d946c
Stephan Kulow <coolo@suse.de>
2010-06-30 21:04:10 +0000

    update translations

--- YaST/Repos/update.sh
+++ YaST/Repos/update.sh
@@ -4,7 +4,7 @@ done
 svn up $MY_LCN_CHECKOUT/50-pot
 xgettext -kN_ -o $MY_LCN_CHECKOUT/50-pot/community-repositories.pot *.h
 svn diff $MY_LCN_CHECKOUT/50-pot/community-repositories.pot
-svn ci $MY_LCN_CHECKOUT/50-pot/community-repositories.pot
+#svn ci $MY_LCN_CHECKOUT/50-pot/community-repositories.pot
 rm *.h
 
 svn up $MY_LCN_CHECKOUT
@@ -16,8 +16,8 @@ for i in $MY_LCN_CHECKOUT/*/po/community-repositories*.po; do
   cp $i po/$file.po
 done
 
-for i in *.xml; do 
-  intltool-merge --xml-style po $i.in $i
+for i in *.xml.in; do 
+  intltool-merge --xml-style po $i ${i/.in/}
 done
 
 svn ci -m "update translations"
